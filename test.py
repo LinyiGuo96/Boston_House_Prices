d7a275ab16e4dfb80a8da0c711f5ea5ba2080ce4
LinyiGuo96 <guoly96@gmail.com>
2022-09-14 22:36:31 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import numpy as np
+import pandas as pd
+import os
 
+# change the work directory
+# os.getcwd()
+os.chdir("E:\GitHubRepo\Boston_House_Prices")
 
-x = 5
-y = 8
-x+y
+# read the training set
+train_data = pd.read_csv("data/train.csv")
+
