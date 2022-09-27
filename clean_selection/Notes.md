95e70137c2958b9b230699d51eb1e2d75c2ec594
LinyiGuo96 <guoly96@gmail.com>
2022-09-26 22:57:10 -0400

    update notes

--- clean_selection/Notes.md
+++ clean_selection/Notes.md
@@ -4,4 +4,14 @@ This file is to save my notes/ideas while learning and exploring.
 
 # Body
 
+**Change the directory of Jupyter Notebook**
+
+```{python}
+jupyter notebook --notebook-dir C:/users/lincoln
+```
+
+**delete objects**
+
+`del` 
+
 
--- test.py
+++ test.py
@@ -1,7 +1,12 @@
+# Import packages
+
 import numpy as np
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+import warnings
+
+
 pd.options.display.width
 desired_width=320
 pd.set_option('display.width', desired_width)
@@ -10,7 +15,9 @@ pd.set_option('display.max_columns',10)
 
 # change the work directory
 os.getcwd()
-os.chdir("E:\GitHubRepo\Boston_House_Prices")
+os.chdir("E://GitHubRepo/Boston_House_Prices")
+print(os.listdir("data")) # list files in data folder
+warnings.filterwarnings('ignore')
 
 # read the training set
 train_data = pd.read_csv("data/train.csv")
@@ -21,5 +28,8 @@ train_data.describe()
 
 # Data Explore by plotting
 
-plt.plot(train_data['LotArea'], train_data['SalePrice'])
+'''this part is replaced by Power BI.'''
+
+
+
 
