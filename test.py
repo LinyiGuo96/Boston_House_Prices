76483bc6d0be98ebeb02840b023ffd05ebc7ab4b
LinyiGuo96 <guoly96@gmail.com>
2022-09-26 23:43:30 -0400

    Update test.py

--- test.py
+++ test.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 import warnings
 
 
+# set up pandas print-out option
 pd.options.display.width
 desired_width=320
 pd.set_option('display.width', desired_width)
-np.set_printoption(linewidth=desired_width)
 pd.set_option('display.max_columns',10)
 
 # change the work directory
@@ -19,16 +19,28 @@ os.chdir("E://GitHubRepo/Boston_House_Prices")
 print(os.listdir("data")) # list files in data folder
 warnings.filterwarnings('ignore')
 
-# read the training set
-train_data = pd.read_csv("data/train.csv")
 
-train_data.head()
 
-train_data.describe()
+#################################
+########    Load Data    ########
+#################################
 
-# Data Explore by plotting
+train = pd.read_csv("data/train.csv")
+test = pd.read_csv("data/test.csv")
 
-'''this part is replaced by Power BI.'''
+train.head()
+test.head()
+
+train.describe()
+test.describe()
+
+train.shape
+test.shape
+
+
+# Data Explore
+
+'''this plotting part is replaced by Power BI.'''
 
 
 
