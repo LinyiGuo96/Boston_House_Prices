34ea5887e6b800b6a59409f830d868d41574c48f
LinyiGuo96 <guoly96@gmail.com>
2023-01-18 21:35:42 -0500

    Update test.py

--- test.py
+++ test.py
@@ -38,6 +38,7 @@ train.shape
 test.shape
 
 
+
 # Data Explore
 
 '''this plotting part is replaced by Power BI.'''
@@ -49,3 +50,56 @@ b = a
 print(b)
 
 
+## validation
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+from sklearn.model_selection import cross_val_score
+
+list = [1,2,3,4,5]
+d = {}
+for x, y in enumerate(list):
+    d[y] = x
+print(d)
+d.values()
+
+
+def twoSum(nums):
+    # d = {}
+    # for x, y in enumerate(nums):
+    #     if target - y in d:
+    #         return [d[target-y], x]
+    #     else:
+    #         d[y] = x
+    d = {}
+    for x, y in enumerate(nums):
+        if target - y in d:
+            return [d[target-y], x]
+        else:
+            d[y] = x
+
+
+x = [1,2,3,4,5]
+sum(x)
+
+
+s = 'LCXXI'
+x = [d[i] for i in s]
+d = {
+    'I':             1,
+    'V':             5,
+    'X':             10,
+    'L':             50,
+    'C':             100,
+    'D':             500,
+    'M':             1000
+}
+
+x = [d[i] for i in list(s)]
+
+list(s)
+
+for i in range(len(x)):
+    print(i)
+
+#%%
